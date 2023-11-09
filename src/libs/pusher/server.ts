bb5de400d88f0c17b3050915f5cf6b23e2b61f99
renatoastra <renatocostatristao@gmail.com>
2023-11-08 21:55:44 -0300

    pusher config added

--- src/libs/pusher/server.ts
+++ src/libs/pusher/server.ts
@@ -4,5 +4,7 @@ export const pusher = new Pusher({
   appId: process.env.SOKETI_DEFAULT_APP_ID!,
   secret: process.env.SOKETI_DEFAULT_APP_SECRET!,
   key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
-  cluster: process.env.NEXT_PUBLIC_SOKETI_CLUSTER!,
+  cluster: "",
+  host: process.env.NEXT_PUBLIC_SOKETI_URL!,
+  port: process.env.NEXT_PUBLIC_SOKETI_PORT!,
 });
